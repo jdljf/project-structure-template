5f4c9972ca70e865f21843672512cff2d0cc4f86
jdljf <1243270526@qq.com>
2019-07-10 20:03:39 +0800

    功能：修改 vue-router 和 vuex 的目录位置

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue"
 import App from "@/App.vue"
-import router from "@/router"
-import store from "@/store"
+import "@/common/css/reset.css"
+import "@/common/css/common.css" // 必须先引用 reset.css 再引入该文件
+import router from "@/router/index.js"
+import store from "@/vuex/index.js"
 import api from "@/http/index.js"
 import "./registerServiceWorker"
 import "@/mock/index.js"
-import "@/common/css/reset.css"
-import "@/common/css/common.css" // 必须先引用 reset.css 再引入该文件
 
 // 把 http 请求绑定到 vue 的原型上
 Object.defineProperty(Vue.prototype, "$api", {
